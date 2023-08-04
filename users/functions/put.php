ce477d58f4d9bc595f4162800787d082fe0a3c38
Alexandre Fernandes <107068042+alexandrectbj@users.noreply.github.com>
2023-08-04 02:00:51 -0300

    FIxed

--- users/functions/put.php
+++ users/functions/put.php
@@ -1,21 +1,21 @@
 <?php
 
-if (isset($_GET['id'])) {
 
-    $id = $conn->real_escape_string($_GET['id']);
-    $data = json_decode(file_get_contents("php://input"));
-    $sql = "UPDATE users
+
+
+$data = json_decode(file_get_contents("php://input"));
+$sql = "UPDATE users
     SET
       nome = '$data->nome',
       sobrenome = '$data->sobrenome',
       email = '$data->email',
       nascimento = '$data->nascimento'
     WHERE
-     id = '$id'";
+     id = '$data->id'";
 
-    if ($conn->query($sql)) {
-        $status = array("status" => "Dados atualizados!");
-    }
+if ($conn->query($sql)) {
+    $status = array("status" => "Dados atualizados!");
 }
+
 
 exit(json_encode($status));
